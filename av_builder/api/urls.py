10499d0cdde0f49073766ceb43474495d3228e37
Tolga Bilbey <bilbeyt@gmail.com>
2024-10-22 08:18:11 +0200

    feat: add tests for api and format api

--- av_builder/api/urls.py
+++ av_builder/api/urls.py
@@ -18,9 +18,16 @@ urlpatterns = [
     path("logout", custom_logout, name="realout"),
     path("auth", auth_view, name="auth"),
     path("api/user/<int:pk>", UserRetrieveAPI.as_view(), name="retrieve-user"),
-    path("api/aircraft-parts", AircraftPartListCreateAPI.as_view(), name="list-create-aircraft-parts"),
-    path("api/aircraft-parts/<int:pk>", AircraftPartDestroyAPI.as_view(), name="destroy-aircraft-parts"),
+    path(
+        "api/aircraft-parts",
+        AircraftPartListCreateAPI.as_view(),
+        name="list-create-aircraft-parts",
+    ),
+    path(
+        "api/aircraft-parts/<int:pk>",
+        AircraftPartDestroyAPI.as_view(),
+        name="destroy-aircraft-parts",
+    ),
     path("api/aircrafts", AircraftListCreateAPI.as_view(), name="list-create-aircrafts"),
-    path("api/aircrafts/<int:pk>", AircraftDestroyAPI.as_view(), name="destroy-aircrafts")
-
+    path("api/aircrafts/<int:pk>", AircraftDestroyAPI.as_view(), name="destroy-aircrafts"),
 ]
--- av_builder/api/views.py
+++ av_builder/api/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_protect
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.generics import DestroyAPIView, ListCreateAPIView, RetrieveAPIView
 
 from aircrafts.models import Aircraft, AircraftPart
@@ -16,31 +16,30 @@ from api.serializers import AircraftPartSerializer, AircraftSerializer, UserSeri
 
 @login_required
 def index(request, *args, **kwargs):
-    return render(request, 'api/index.html')
+    return render(request, "api/index.html")
+
 
 @csrf_protect
 def custom_login(request, *args, **kwargs):
     messages.success(request, "You are successfully logged in")
-    return render(request, 'api/login.html')
+    return render(request, "api/login.html")
 
 
 def custom_logout(request):
     logout(request)
-    messages.success(
-        request, "You have logged out successfully.")
+    messages.success(request, "You have logged out successfully.")
     return HttpResponseRedirect(reverse("base"))
 
 
 def auth_view(request):
-    username = request.POST.get('username' , '')
-    password = request.POST.get('password' , '')
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
     user = authenticate(username=username, password=password)
 
     if user is not None:
-       login(request, user)
-       return HttpResponseRedirect(reverse("base"))
-    else:
-       return HttpResponseRedirect(reverse("login"))
+        login(request, user)
+        return HttpResponseRedirect(reverse("base"))
+    return HttpResponseRedirect(reverse("login"))
 
 
 class UserRetrieveAPI(RetrieveAPIView):
@@ -52,18 +51,25 @@ class UserRetrieveAPI(RetrieveAPIView):
 class AircraftPartListCreateAPI(ListCreateAPIView):
     authentication_classes = [BasicAuthentication, SessionAuthentication]
     serializer_class = AircraftPartSerializer
-    
+
     def get_queryset(self):
         part_name_value = self.request.user.groups.first().name
         return AircraftPart.objects.filter(name=part_name_value)
+
+    def perform_create(self, serializer):
+        if serializer.validated_data["name"] != self.request.user.groups.first().name:
+            raise PermissionDenied("Can not create other parts")
+        return super().perform_create(serializer)
 
 
 class AircraftPartDestroyAPI(DestroyAPIView):
     authentication_classes = [BasicAuthentication, SessionAuthentication]
     queryset = AircraftPart.objects.all()
     serializer_class = AircraftPartSerializer
-    
+
     def perform_destroy(self, instance):
+        if instance.name != self.request.user.groups.first().name:
+            raise PermissionDenied("Can not delete other parts")
         if instance.aircraft:
             raise ValidationError(f"Can not delete {instance.name}. It is used.")
         return super().perform_destroy(instance)
@@ -74,8 +80,23 @@ class AircraftListCreateAPI(ListCreateAPIView):
     serializer_class = AircraftSerializer
     queryset = Aircraft.objects.all()
 
+    def get_queryset(self):
+        if self.request.user.groups.first().name != "Assembly":
+            raise PermissionDenied("can not list aircrafts")
+        return super().get_queryset()
+
+    def perform_create(self, serializer):
+        if self.request.user.groups.first().name != "Assembly":
+            raise ValidationError("Can not create aircraft")
+        return super().perform_create(serializer)
+
 
 class AircraftDestroyAPI(DestroyAPIView):
     authentication_classes = [BasicAuthentication, SessionAuthentication]
     queryset = Aircraft.objects.all()
     serializer_class = AircraftSerializer
+
+    def perform_destroy(self, instance):
+        if self.request.user.groups.first().name != "Assembly":
+            raise PermissionDenied("Can not destroy aircraft")
+        return super().perform_destroy(instance)
